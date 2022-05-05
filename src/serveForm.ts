258bd0c3c9b37d895c4c5739e35b5936a9f7d4e0
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-05-05 09:47:38 -0500

    feat: updated login landing page to enforce correct callbacks

--- src/serveForm.ts
+++ src/serveForm.ts
@@ -2,19 +2,40 @@ import { ServerResponse } from 'http';
 import fs from 'fs/promises';
 import ejs from 'ejs';
 import * as path from 'path';
+import { URL } from 'url';
 
 import apps from './apps';
 import globalStyles from './globalStyles';
 import logo from './logo';
 
 const serveForm = (form) => async (req, res: ServerResponse) => {
+  const { callbackURL, app } = req.query;
   const file = await fs.readFile(path.join(__dirname, '../templates', `${form}.html`));
+
+  const appInfo = apps[app];
+
+  if (callbackURL && !app) {
+    const url = new URL(callbackURL);
+    url.searchParams.set('error', 'NoAppError');
+    res.setHeader('Location', url.toString());
+    res.statusCode = 307;
+    return res.end();
+  }
+
+  if (callbackURL && !callbackURL.startsWith(appInfo[app]?.callbackURL)) {
+    const url = new URL(callbackURL);
+    url.searchParams.set('error', 'InvalidCallbackError');
+    res.setHeader('Location', url.toString());
+    res.statusCode = 307;
+    return res.end();
+  }
+
   const html = ejs.render(file.toString(), {
     // @ts-ignore
     csrf: req.csrfToken(),
-    appName: apps[req.query.app]?.appName || 'Flatland Church',
-    app: req.query.app,
-    callbackURL: req.query.callbackURL,
+    appName: appInfo?.appName || 'Flatland Church',
+    app,
+    callbackURL: callbackURL || appInfo[app]?.callbackURL,
     globalStyles,
     searchParams: req.search || '',
     logo,
